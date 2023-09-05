8d4e516e3415cb3b2159e8dcf044b7d9e5d57019
Etec Prof. Horácio Augusto da Silveira <caioquir@gmail.com>
2023-09-04 22:30:14 -0300

    Arrumado

--- Exceptions/NotFoundException.cs
+++ Exceptions/NotFoundException.cs
@@ -1,6 +1,29 @@
-﻿namespace OsDsII.api.Exceptions
+﻿using System.Net;
+
+namespace OsDsII.api.Exceptions
 {
-    public class NotFoundException
+    public class NotFoundException : BaseException
     {
+        public NotFoundException(string resource) :
+            base(
+                "ERROR:001",
+                $"{resource} not found",
+                HttpStatusCode.NotFound,
+                StatusCodes.Status404NotFound,
+                DateTimeOffset.UtcNow,
+                null
+            )
+        { }
+
+        public NotFoundException(string message, Exception ex) :
+            base(
+                "ERROR:001",
+                message,
+                HttpStatusCode.NotFound,
+                StatusCodes.Status404NotFound,
+                DateTimeOffset.UtcNow,
+                ex
+            )
+        { }
     }
 }
--- Services/Customers/CustomersService.cs
+++ Services/Customers/CustomersService.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using OsDsII.api.data;
 using OsDsII.api.Models;
+using OsDsII.api.Repositories.Interfaces;
 using OsDsII.api.Repositories.UnitOfWork;
+using OsDsII.api.Services.Interfaces;
 
 namespace OsDsII.api.Services
 {
